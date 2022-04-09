d6ca450f3bd04214980cf1c9103db9fcf95258b7
Ouali98 <ridaouali98@gmail.com>
2022-04-09 15:35:20 +0000

    delete trash fils

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 100, 'easeInOutExpo');
         return false;
     });
 
